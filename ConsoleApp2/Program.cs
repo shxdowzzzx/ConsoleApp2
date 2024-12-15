a1c6057248f083949f10c7104befe12e05efd3ef
Alex Tilic <alextilic@gmail.com>
2024-12-15 18:03:58 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Angajat("Alexei", 8250, "$", "IT");
+            var b = new Angajat("Daniel", 7500, "$", "Marketing");
+            var c = new Angajat("Mihai", 5000, "$", "Resurse Umane");
+
+            a.AfisareDetalii();
+            b.AfisareDetalii();
+            c.AfisareDetalii();
+
+        }
+    }
+}
---
+++ ConsoleApp2/baze.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Angajat
+    {
+        string Nume;
+        double Salariu;
+        string Departament;
+        string Valuta;
+        public Angajat(string nume, double salariu, string valuta, string departament)
+        {
+            Nume = nume;
+            Salariu = salariu;
+            Valuta = valuta;
+            Departament = departament;
+
+        }
+
+        public void AfisareDetalii()
+        {
+            Console.WriteLine($"Numele angajatului este {Nume}, salariul de {Salariu} {Valuta}, departamentul {Departament}.");
+        }
+
+
+
+    }
+}
